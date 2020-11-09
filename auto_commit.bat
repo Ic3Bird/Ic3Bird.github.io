69de6cb9fbfaea70c38d96eb960120b9925a2733
Ic3Bird <davidtugyi@indamail.hu>
2020-11-09 23:18:08 +0100

    Auto-committed on 2020-11-09

--- auto_commit.bat
+++ auto_commit.bat
@@ -4,12 +4,8 @@ echo type "commit" or "update"
 cd "curl"
 
 :: set GIT_PATH="C:\Program Files (x86)\Git\bin\git.exe"
-<<<<<<< HEAD
 :: set GIT_PATH="C:\Users\didtugyi\AppData\Local\Programs\Git\cmd\git.exe"
 set GIT_PATH="C:\Program Files\Git\cmd\git.exe"
-=======
-set GIT_PATH="C:\Users\didtugyi\AppData\Local\Programs\Git\cmd\git.exe"
->>>>>>> 58dc1f9cd5237cabf2988fea6d541c3667c1a6a5
 set BRANCH = "origin"
 
 :P
