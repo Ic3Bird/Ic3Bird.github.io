23037d3b5bc9dbfe7587152b9f78df666d678348
Ic3Bird <davidtugyi@indamail.hu>
2021-02-11 11:15:07 +0100

    Auto-committed on 2021-02-11

--- js/Common/serverSettings.js
+++ js/Common/serverSettings.js
@@ -2,7 +2,7 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     function myAPIsource() {
-        return "http://localhost:60694";
+        return "https://snowart.azurewebsites.net";
     }
     exports.myAPIsource = myAPIsource;
 });
