db56a0aa2b160bf6a3fc2c3508048d4a354da10d
Ic3Bird <davidtugyi@indamail.hu>
2021-02-11 15:13:05 +0100

    Auto-committed on 2021-02-11

--- js/Common/serverSettings.js
+++ js/Common/serverSettings.js
@@ -2,7 +2,10 @@ define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     function myAPIsource() {
-        return "https://snowart.azurewebsites.net";
+        if (window.location.href.includes('ic3bird.github.io')) {
+            return "https://snowart.azurewebsites.net";
+        }
+        return "http://localhost:60694";
     }
     exports.myAPIsource = myAPIsource;
 });
